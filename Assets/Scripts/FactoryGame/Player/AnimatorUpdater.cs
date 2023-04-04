de39370664a7ebef3fe95efa15a5be3a81b16170
AlexProgrammer <alexprogrammer74@gmail.com>
2023-04-05 01:10:11 +0500

    Player character

---
+++ Assets/Scripts/FactoryGame/Player/AnimatorUpdater.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace FactoryGame.Player
+{
+    public class AnimatorUpdater : MonoBehaviour
+    {
+        [SerializeField] private UserInput userInput;
+
+        private Animator _animator;
+        private int _walkingLayerIndex;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _walkingLayerIndex = _animator.GetLayerIndex("Walking Layer");
+        }
+
+        private void Update()
+        {
+            _animator.SetLayerWeight(_walkingLayerIndex, userInput.Joystick.magnitude);
+            if (userInput.Joystick != Vector2.zero)
+            {
+                Vector2 inputDelta = userInput.Joystick;
+                Vector3 relativeDeltaPos = new Vector3(inputDelta.x, 0, inputDelta.y);
+                Quaternion cameraRotation = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0);
+                transform.rotation = Quaternion.LookRotation(cameraRotation * relativeDeltaPos);
+            }
+        }
+    }
+}
